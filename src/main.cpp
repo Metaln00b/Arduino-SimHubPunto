fb1d187a093dea2dd02687a70484d53282f57c21
Florian <Metaln00b@web.de>
2024-02-19 22:08:52 +0100

    Small code cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <mcp_can.h>
-
 #include <SPI.h>
 
-//#define CAN0_INT 2 // Set INT to pin 2
-MCP_CAN CAN0(10); // Set CS to pin 10
+MCP_CAN CAN0(10); // Set CS Pin
 
 #define BUF_SIZE                    64
 
@@ -13,15 +11,15 @@ MCP_CAN CAN0(10); // Set CS to pin 10
 char simhub_message_buf[BUF_SIZE];
 
 void splitBytes(uint16_t num, uint8_t *lowByte, uint8_t *highByte) {
-    *lowByte = (uint8_t)(num & 0xFF);        // Niedrigeres Byte
-    *highByte = (uint8_t)((num >> 8) & 0xFF); // Höheres Byte
+    *lowByte = (uint8_t)(num & 0xFF);
+    *highByte = (uint8_t)((num >> 8) & 0xFF);
 }
 
 void setup() {
     Serial.begin(115200);
     SPI.begin();
 
-    if(CAN0.begin(MCP_ANY, CAN_50KBPS, MCP_8MHZ) == CAN_OK)
+    if (CAN0.begin(MCP_ANY, CAN_50KBPS, MCP_8MHZ) == CAN_OK)
     {
         Serial.println("MCP2515 Initialized Successfully!");
         delay(1000);
@@ -33,35 +31,8 @@ void setup() {
         setup();
     }
     CAN0.setMode(MCP_NORMAL);
-    
-    //pinMode(CAN0_INT, INPUT);
 
     memset(simhub_message_buf, 0x0, BUF_SIZE);
-}
-
-void dontloop() {
-    byte data180[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    CAN0.sendMsgBuf(0x180, 0, 6, data180);
-
-    byte data282[8] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-    CAN0.sendMsgBuf(0x282, 0, 8, data282);
-
-    byte data281[8] = {0x00, 0x00, 0x80, 0x80, 0x00, 0x00, 0x40, 0x00};
-    CAN0.sendMsgBuf(0x281, 0, 8, data281);
-
-    byte data2a0[4] = {0x00, 0xAF, 0x00, 0xCA};
-    CAN0.sendMsgBuf(0x2a0, 0, 4, data2a0);
-
-    byte data380[8] = {0x08, 0x00, 0x40, 0x00, 0x00, 0x64, 0x00, 0x00};
-    CAN0.sendMsgBuf(0x380, 0, 8, data380);
-
-    byte data286[2] = {0x00, 0x00};
-    CAN0.sendMsgBuf(0x286, 0, 2, data286);
-
-    byte data382[1] = {0x00};
-    CAN0.sendMsgBuf(0x382, 0, 1, data382);
-
-    delay(100);
 }
 
 void process_message() {
@@ -161,28 +132,5 @@ void loop() {
         simhub_message_buf[min(readCount, BUF_SIZE - 1)] = 0x0;
         process_message();
         memset(simhub_message_buf, 0x0, BUF_SIZE);
-        //delay(100);
     }
 }
-/*
-90  -> 50°C
-98  -> 60°C
-106 -> 70°C
-113 -> 80°C
-
-147 -> 100°C
-155 -> 110°C
---------------
-50°C  -> 90
-60°C  -> 98
-70°C  -> 106
-80°C  -> 113
-
-
-90°C  -> 121 - 140 | 130
-continues at 141 for 91°C (temp * 0.8 + 50)
-
-
-100°C -> 147 (temp * 0.8 + 67)
-110°C -> 155
-*/
